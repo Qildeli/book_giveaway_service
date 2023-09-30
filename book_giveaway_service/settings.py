db14ea0954d9a8a3fc256b1bc201e23fdcffe230
Qildeli <anaqildeli@gmail.com>
2023-10-01 02:27:07 +0400

    Set up PostgreSQL as the primary database for the Django app

--- book_giveaway_service/settings.py
+++ book_giveaway_service/settings.py
@@ -94,8 +94,12 @@ WSGI_APPLICATION = 'book_giveaway_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'book_giveaway_service',
+        'USER': 'postgres',
+        'PASSWORD': 'test123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
