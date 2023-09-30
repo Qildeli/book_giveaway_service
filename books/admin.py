5a1e50c243b73d2f65aebba2d103f1c6d87e5d67
Qildeli <anaqildeli@gmail.com>
2023-09-30 15:13:37 +0400

    Register Book-related models with the admin site

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Book, Author, Genre, BookCondition, PickupLocation, BookRequest
 
-# Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookCondition)
+admin.site.register(PickupLocation)
+admin.site.register(BookRequest)
+
