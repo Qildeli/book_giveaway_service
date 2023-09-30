97f6a8314bd50d0ccff5eed1bf315bd0956960d1
Qildeli <anaqildeli@gmail.com>
2023-09-30 15:12:36 +0400

    Setup Swagger and Redoc documentation for Book Giveaway Service API

--- book_giveaway_service/urls.py
+++ book_giveaway_service/urls.py
@@ -15,8 +15,45 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import permissions
+from rest_framework.authentication import TokenAuthentication
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Book Giveaway Service API",
+        default_version='v1',
+        description="API documentation for the Book Giveaway Service",
+        x={
+            "securityDefinitions": {
+                "Token": {
+                    "type": "apiKey",
+                    "name": "Authorization",
+                    "in": "header",
+                    "description": "Enter your token in the format: Token <token>"
+                }
+            },
+            "security": [{"Token": []}],
+        }
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+    authentication_classes=(TokenAuthentication,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('users.urls')),
+    path('api/', include('books.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
